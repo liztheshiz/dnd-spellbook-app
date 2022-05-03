b77111810f42a33308983e5c3ba2bde5850eab4c
liztheshiz <65947305+liztheshiz@users.noreply.github.com>
2022-05-03 14:29:37 -0400

    improved pokemonList init

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,5 @@
-let pokemonList = [];
-
-pokemonList[0] = {
-	name: 'Charizard',
-	height: 1.7,
-	types: ['fire', 'flying']
-};
-
-pokemonList[1] = {
-	name: 'Mankey',
-	height: 0.5,
-	types: ['fighting']
-};
-
-pokemonList[2] = {
-	name: 'Piloswine',
-	height: 1.1,
-	types: ['ice', 'ground']
-};
+let pokemonList = [
+	{name: 'Charizard', height: 1.7, types: ['fire', 'flying']},
+	{name: 'Mankey', height: 0.5, types: ['fighting']},
+	{name: 'Piloswine', height: 1.1, types: ['ice', 'ground']}
+];
