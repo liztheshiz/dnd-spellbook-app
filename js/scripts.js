0622843cc0484eb7a0df1f067bbb9540e3bc05f0
liztheshiz <65947305+liztheshiz@users.noreply.github.com>
2022-05-03 15:41:53 -0400

    made list of pokemon print on screen

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
+// Initialize list of Pokemon
 let pokemonList = [
 	{name: 'Charizard', height: 1.7, types: ['fire', 'flying']},
 	{name: 'Mankey', height: 0.5, types: ['fighting']},
 	{name: 'Piloswine', height: 1.1, types: ['ice', 'ground']}
 ];
+
+// Write list of Pokemon in pokemonList on screen
+for (let i = 0; i < 3; i++) {
+	document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height}); `);
+}
