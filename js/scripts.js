6a54dc2a42b403dba742f3b18c870e37ec50b841
liztheshiz <65947305+liztheshiz@users.noreply.github.com>
2022-05-06 15:59:09 -0400

    wrapped pokemonList in IIFE w methods

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,24 @@
-// Initialize list of Pokemon
-let pokemonList = [
-	{name: 'Charizard', height: 1.7, types: ['fire', 'flying']},
-	{name: 'Mankey', height: 0.5, types: ['fighting']},
-	{name: 'Piloswine', height: 1.1, types: ['ice', 'ground']}
-];
+// Initialize list of Pokemon + methods in IIFE
+let pokemonRepository = (function() {
+	let pokemonList = [
+		{name: 'Charizard', height: 1.7, types: ['fire', 'flying']},
+		{name: 'Mankey', height: 0.5, types: ['fighting']},
+		{name: 'Piloswine', height: 1.1, types: ['ice', 'ground']}
+	];
+
+	function getAll() {
+		return pokemonList;
+	}
+
+	function add(item){
+		pokemonList.push(item);
+	}
+
+	return {add, getAll}
+})();
 
 // Print list of Pokemon in pokemonList in pokemon-list
-pokemonList.forEach(function(pokemon) {
+pokemonRepository.getAll().forEach(function(pokemon) {
 	document.getElementById('pokemon-list').innerHTML += `${pokemon.name} (height: ${pokemon.height})`;
 	if (pokemon.height >= 1.5) {
 		document.getElementById('pokemon-list').innerHTML += ` - Wow, that's big!`;
