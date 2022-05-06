a7a740b42d456dec9a4ec2e2aba6fa8eda3b4945
liztheshiz <65947305+liztheshiz@users.noreply.github.com>
2022-05-06 12:19:25 -0400

    changed for loop to forEach function, print list in div

--- js/scripts.js
+++ js/scripts.js
@@ -5,11 +5,11 @@ let pokemonList = [
 	{name: 'Piloswine', height: 1.1, types: ['ice', 'ground']}
 ];
 
-// Write list of Pokemon in pokemonList on screen
-for (let i = 0; i < pokemonList.length; i++) {
-	document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height})`);
-	if (pokemonList[i].height >= 1.5) {
-		document.write(` - Wow, that's big!`);
+// Print list of Pokemon in pokemonList in pokemon-list
+pokemonList.forEach(function(pokemon) {
+	document.getElementById('pokemon-list').innerHTML += `${pokemon.name} (height: ${pokemon.height})`;
+	if (pokemon.height >= 1.5) {
+		document.getElementById('pokemon-list').innerHTML += ` - Wow, that's big!`;
 	}
-	document.write('<br>');
-}
+	document.getElementById('pokemon-list').innerHTML += '<br>';
+});
